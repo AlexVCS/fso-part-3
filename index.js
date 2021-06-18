e9e6da4a596118204795c593cc93c35add444c86
Alex Curtis-Slep <alexcurtisslep@gmail.com>
2021-06-18 19:10:12 -0400

    setting up repo for part 3

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "050-3432"
+  },
+  {
+    id: 2,
+    name: "Gentry Guthry",
+    number: "320-2194"
+  },
+  {
+    id: 3,
+    name: "Nove Chin",
+    number: "324-2390"
+  },
+  {
+    id: 4,
+    name: "Sindus Minsky",
+    number: "239-3201"
+  }
+]
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+let ts = Date.now();
+
+let date = new Date()
+
+app.get('/info', (request, response) => {
+  response.send(`<p>Phonebook has info for ${persons.length} people</p>
+                 <p> ${date} </p>`)
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
