1db844770dde3aca99cdebf933797844430f7957
Alex Curtis-Slep <alexcurtisslep@gmail.com>
2021-06-19 07:20:16 -0400

    Working on logic to fetch a single resource

--- index.js
+++ index.js
@@ -32,6 +32,17 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 let ts = Date.now();
 
 let date = new Date()
@@ -42,5 +53,6 @@ app.get('/info', (request, response) => {
 })
 
 const PORT = 3001
-app.listen(PORT)
+app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
+})
