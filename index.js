0fcfc828c90f95ad738caad744efad70b42019a2
Alex Curtis-Slep <alexcurtisslep@gmail.com>
2021-06-28 11:06:43 -0400

    Working on error handling if name already exists

--- index.js
+++ index.js
@@ -65,8 +65,23 @@ app.get('/info', (request, response) => {
 
 app.post('/api/persons', (request, response) => {
   const person = request.body
+  console.log(person)
+  if (!person.name || !person.phone) {
+    return response.status(400).json({
+      error: 'Each entry must have a name and phone number'
+    })
+  }
+
+  if (person.name !== undefined && person.name !== null) {
+    return response.status(400).json({
+      error: 'Each name must be unique'
+    })
+  }
+
   person.id = Math.floor(Math.random() * 200)
   response.json(person)
+
+  
 })
 
 const PORT = 3001
